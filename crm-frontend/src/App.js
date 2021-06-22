bfd87bfcb1ffd748f02f781bc6d9cb91759af725
Kadiyala Chaitanya <you@example.com>
2021-06-22 14:34:26 +0530

    dashboard page added

--- crm-frontend/src/App.js
+++ crm-frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Entry } from "./page/entry/Entry.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./page/dashboard/Dashboard.page";
 
 function App() {
   return (
     <div>
       {/* <Entry /> */}
-      <DefaultLayout>//Dashboard</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
